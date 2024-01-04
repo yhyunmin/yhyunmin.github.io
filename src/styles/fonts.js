73f74095657eb5c936677378c8c7e596a1bd7d9d
smurf <smurf@kakao.com>
2024-01-05 03:11:23 +0900

    font optimizer css added

--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,11 +1,17 @@
 import { css } from 'styled-components';
 
-import PretendardBold from '../assets/fonts/Pretendard-Bold.woff2';
-import PretendardRegular from '../assets/fonts/Pretendard-Regular.woff2';
+import PretendardBold from '@font/Pretendard-Bold.woff2';
+import PretendardRegular from '@font/Pretendard-Regular.woff2';
+import PretendardSemiBold from '@font/Pretendard-SemiBold.woff2';
+import D2Coding from '@font/D2Coding.woff2';
 
 export const font = css`
   @font-face {
     font-family: 'Pretendard';
-    src: url();
+    font-style: normal;
+    font-weight: normal;
+    /* 폰트최적화를위한 swap */
+    font-display: swap;
+    src: url('');
   }
 `;
