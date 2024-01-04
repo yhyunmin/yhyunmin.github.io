495c3918d087179689aeee759efc6b38577bf388
smurf <smurf@kakao.com>
2024-01-05 02:55:24 +0900

    added craco

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 
 function App() {
-  return <div className='App'>이현민의 블로그</div>;
+  return (
+    <div className='App'>
+      <h1>안녕하세요 이현민의 블로그입니다.</h1>
+      <p>현재 제작중에 있습니다</p>
+    </div>
+  );
 }
 
 export default App;
--- src/styles/fonts.js
+++ src/styles/fonts.js
@@ -1,1 +1,11 @@
 import { css } from 'styled-components';
+
+import PretendardBold from '../assets/fonts/Pretendard-Bold.woff2';
+import PretendardRegular from '../assets/fonts/Pretendard-Regular.woff2';
+
+export const font = css`
+  @font-face {
+    font-family: 'Pretendard';
+    src: url();
+  }
+`;
