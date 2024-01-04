c3bf29bb7b7c1c090151aa6fb3daa562e38e4d9b
smurf <smurf@kakao.com>
2024-01-05 04:03:43 +0900

    feat : components

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
         <h4>1.8rem</h4>
         <p>p와 span은 1.6rem</p>
         <p>현재 제작중에 있습니다</p>
+
         <button onClick={onClickBtn}>다크모드</button>
       </ThemeProvider>
     </div>
---
+++ src/components/tag/Tag.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const tag = () => {
+  return <div>tag</div>;
+};
