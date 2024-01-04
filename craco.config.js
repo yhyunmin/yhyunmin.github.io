f674c1c2c832266fdc3a647f59f4586ef5ddcdb2
smurf <smurf@kakao.com>
2024-01-05 02:58:43 +0900

    @import

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+const CracoAlias = require('craco-alias');
+module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: { source: 'jsconfig', tsConfigPath: './jsconfig.paths.json' },
+    },
+  ],
+};
