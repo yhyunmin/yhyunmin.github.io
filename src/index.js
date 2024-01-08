7ad08584346a994023258bf8354db924518b2e2d
smurf <smurf@kakao.com>
2024-01-09 00:11:42 +0900

    add loader fallbackelements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+import { RouterProvider } from 'react-router-dom';
+import router from './router/routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <RouterProvider router={router} fallbackElement={<div>Loading</div>}>
+    <App />
+  </RouterProvider>
+);
--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -7,6 +7,13 @@ const router = createHashRouter(
     {
       path: '/',
       element: <App />,
+      loader: async () => {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            return resolve('loading done');
+          }, 1000);
+        });
+      },
       children: [{ path: '/:postId', element: <Post /> }],
     },
   ],
