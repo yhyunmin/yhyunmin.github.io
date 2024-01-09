094ce61cfd487fdfdd626c8368c6257e6501b525
smurf <smurf@kakao.com>
2024-01-10 06:52:19 +0900

    fix:infinite loading

--- src/components/Atoms/DarkTheme/DarkTheme.jsx
+++ src/components/Atoms/DarkTheme/DarkTheme.jsx
@@ -4,7 +4,7 @@ import useThemeStore from 'src/store/ThemeStore';
 const DarkTheme = () => {
   const { toggleTheme } = useThemeStore();
 
-  return <button onClick={toggleTheme()}>다크모드</button>;
+  return <button onClick={toggleTheme}>다크모드</button>;
 };
 
 export default DarkTheme;
