698435609c331cab7b0785c29206c4fee7983e3f
smurf <smurf@kakao.com>
2024-01-14 02:08:13 +0900

    wip

--- src/components/Molecules/ArticleList/ArticleList2.jsx
+++ src/components/Molecules/ArticleList/ArticleList2.jsx
@@ -1,29 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import fm from 'front-matter';
-import Markdown from 'markdown-to-jsx';
 const ArticleList2 = () => {
-  const [content, setContent] = useState([]);
+  const [content, setContent] = useState();
   const mdfiles = require.context('../../../contents', false, /\.md$/).keys();
+  // useEffect(() => {
+  //   mdfiles.map(mdfile => {
+  //     const mdFile = mdfile.replace(/^\.\//, '');
+  //     console.log(mdFile);
+  //     import(`../../../contents/${mdFile}`).then(res => {
+  //       fetch(res.default)
+  //         .then(res => res.text())
+  //         .then(res => setContent([...content, res]))
+  //         .catch(err => console.log(err));
+  //       console.log(res.default, 'default');
+  //     });
+  //   });
+  //   console.log(content, 'conte2nt');
+  // }, []);
   useEffect(() => {
-    mdfiles.map(mdfile => {
-      const mdFile = mdfile.replace(/^\.\//, '');
-      console.log(mdFile);
-      import(`../../../contents/${mdFile}`).then(res => {
-        fetch(res.default)
-          .then(res => res.text())
-          .then(res => setContent([...content, res]))
-          .catch(err => console.log(err));
-        console.log(res.default, 'default');
-      });
-    });
-    console.log(content, 'conte2nt');
-  }, []);
+    const md = require('../../../contents/2023-05-20--test.md');
+    console.log(md);
 
-  return (
-    <div>
-      <Markdown>{content}</Markdown>
-    </div>
-  );
+    fetch(md)
+      .then(res => res.text()) // Return the text Promise
+      .then(text => {
+        setContent(text);
+        console.log(content, 'content1');
+        fm(content);
+        console.log(fm(content).attributes);
+      });
+  }, [content]);
+  return <div></div>;
 };
 
 export default ArticleList2;
