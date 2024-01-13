b0f1ccedfdccb57f408ee75e4a6f596574919cc6
smurf <smurf@kakao.com>
2024-01-13 18:20:27 +0900

    test

--- src/components/Molecules/ArticleList/ArticleList.jsx
+++ src/components/Molecules/ArticleList/ArticleList.jsx
@@ -1,8 +1,8 @@
 import Article from '@components/Atoms/Article/Article';
 import { React, useEffect } from 'react';
 import frontMatter from 'front-matter';
+import test2 from '../../../contents/2023-05-20--test.md';
 // // contextMd로 md파일들을 불러와 불러온 파일들을 순회하며 처리함
-// import test from '../../../contents/2023-05-20--test.md';
 // useEffect(() => {
 //   fetch(test).then(response => {
 //     response.text().then(text => {
@@ -12,19 +12,50 @@ import frontMatter from 'front-matter';
 //     });
 //   });
 // }, []);
+// const importAllMdFiles = async () => {
+//   const mdFiles = await Promise.all(
+//     require
+//       .context('../../../contents', false, /\.md$/)
+//       .keys()
+//       .map(async filename => {
+//         const test = filename.replace(/^\.\//, '');
+//         const fileContent = await import(`../../../contents/${test}`);
+//         const filePath = `../../../contents/${test}`;
+//         const test3 = import(filePath);
+//         const response = await fetch(filePath);
+//         const test2 = frontMatter();
+//         console.log(filePath, response);
+//         return { filename, content: fileContent, test2, test3 };
+//       })
+//   );
+// };
 
-const ArticleList = () => {
-  const mdFilesContext = require.context('../../../contents/', false, /\.md$/);
-  const mdFilesKeys = mdFilesContext.keys();
-  mdFilesKeys.forEach(key => {
-    const mdFile = mdFilesContext(key);
-    console.log(mdFile);
-    console.log(frontMatter(mdFile));
+// importAllMdFiles 함수 호출
+// importAllMdFiles();
+
+const ArticleList = props => {
+  useEffect(() => {
+    fetch(test2).then(response => {
+      response.text().then(text => {
+        const test2 = frontMatter(text);
+        console.log(test2.attributes);
+        console.log(test2.body);
+      });
+    });
+  }, []);
+  useEffect(() => {
+    fetch('../../../contents/2023-05-20--redux.md').then(response => {
+      response.text().then(text => {
+        console.log(text);
+        const test2 = frontMatter(text);
+        console.log(test2.attributes);
+        console.log(test2.body);
+      });
+    });
   }, []);
   return (
     <ul>
       <li>
-        {}
         <Article />
       </li>
     </ul>
