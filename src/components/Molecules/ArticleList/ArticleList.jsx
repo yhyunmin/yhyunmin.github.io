a9c122feea520ffddfa5761c7b3b2fc3b7cb1474
smurf <smurf@kakao.com>
2024-01-17 05:23:20 +0900

    feat : pagination

--- src/components/Molecules/ArticleList/ArticleList.jsx
+++ src/components/Molecules/ArticleList/ArticleList.jsx
@@ -1,21 +1,7 @@
 import React, { useEffect } from 'react';
 import Article from '@components/Atoms/Article/Article';
 import { useFrontMatter, useFrontMatterFetch } from 'src/store/ArticleStore';
-const ArticleList = () => {
-  const frontMatterDatas = useFrontMatter();
-  const fetchFrontMatter = useFrontMatterFetch();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetchFrontMatter();
-    };
-    fetchData()
-      .then(() => {
-        console.log('done');
-      })
-      .catch(error => console.log(error));
-  }, [fetchFrontMatter]);
-
+const ArticleList = ({ frontMatterDatas }) => {
   return (
     <div>
       {frontMatterDatas &&
--- src/components/Organisms/ArticleSection/ArticleSection.jsx
+++ src/components/Organisms/ArticleSection/ArticleSection.jsx
@@ -1,10 +1,46 @@
 import ArticleList from '@components/Molecules/ArticleList/ArticleList';
-import React from 'react';
+import { useFrontMatter, useFrontMatterFetch } from 'src/store/ArticleStore';
+import React, { useEffect, useState } from 'react';
 
 const ArticleSection = ({ className }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const frontMatterDatas = useFrontMatter();
+  const fetchFrontMatter = useFrontMatterFetch();
+
+  const numberItems = 6;
+  const totalPages = frontMatterDatas.length / numberItems;
+  const handleNextPage = () => {
+    if (!(currentPage > 1)) {
+      return null;
+    }
+    setCurrentPage(prev => prev + 1);
+  };
+  const handlePrevPage = () => {
+    if (!(currentPage < totalPages)) {
+      return null;
+    }
+    setCurrentPage(prev => prev - 1);
+  };
+  const startIndex = (currentPage - 1) * numberItems;
+  const endIndex = startIndex + numberItems;
+  const currentItems = frontMatterDatas.slice(startIndex, endIndex);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchFrontMatter();
+    };
+    fetchData()
+      .then(() => {
+        console.log('done');
+      })
+      .catch(error => console.log(error));
+  }, [fetchFrontMatter]);
+
   return (
     <main className={className}>
-      <ArticleList />
+      <ArticleList frontMatterDatas={currentItems} />
+      <button>next</button>
+      <button>prev</button>
     </main>
   );
 };
