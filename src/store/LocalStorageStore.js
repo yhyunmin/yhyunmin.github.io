9cfd74a84f7f7085e27de2028e2360326fe932ee
smurf <smurf@kakao.com>
2024-01-27 04:03:23 +0900

    feat : locastorage store

--- src/store/LocalStorageStore.js
+++ src/store/LocalStorageStore.js
@@ -4,11 +4,18 @@ import { persist, createJSONStorage } from 'zustand/middleware';
 const useLocalStorageStore = create(
   persist(
     (set, get) => ({
-      recentPost: [],
-      setRecentPost: data => {
-        set({ recentPost: data });
+      localStorage: [],
+      action: {
+        setLocalStorage: data => {
+          set({ recentPost: data });
+        },
       },
     }),
     { name: 'recentPost', storage: createJSONStorage(() => localStorage) }
   )
 );
+
+export const useRecentPostStore = () =>
+  useLocalStorageStore(state => state.recentPost);
+export const useRecentPostAction = () =>
+  useLocalStorageStore(state => state.action.setLocalStorage);
