970f6647e6eb4ce6b8bfb4b908c01161c620d36a
smurf <smurf@kakao.com>
2024-01-29 23:25:43 +0900

    style : taglist

--- src/components/Atoms/Tag/Tag.jsx
+++ src/components/Atoms/Tag/Tag.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
 import { styled } from 'styled-components';
 
-const StyledTag = styled.button`
+const StyledTag = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.2rem;
   border: 0.1rem solid #eee;
   border-radius: 5px;
   padding: 4px 8px;
-  opacity: 0.9;
   text-transform: capitalize;
-  font-size: 1.4rem;
-  font-weight: 400;
+  border-color: ${props => props.theme.borderColor};
+  background-color: ${props => props.theme.elavateColor};
+  p {
+    opacity: 0.9;
+    font-size: 1.4rem;
+  }
   &:hover {
     opacity: 1;
   }
   span {
-    font-size: 1.4rem;
+    opacity: 0.7;
+    font-size: 1.2rem;
+    line-height: 1.2rem;
   }
 `;
 const Tag = ({ title, count }) => {
   return (
     <li>
       <StyledTag className='borderColor'>
-        {title} <span>({count})</span>
+        <p>{title}</p>
+        <span>({count})</span>
       </StyledTag>
     </li>
   );
