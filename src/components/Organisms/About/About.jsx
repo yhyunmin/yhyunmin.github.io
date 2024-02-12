850f536c4ee3885c0895f97b36c78bdf681a72d1
smurf <smurf@kakao.com>
2024-02-12 21:27:45 +0900

    content: post

--- src/components/Organisms/About/About.jsx
+++ src/components/Organisms/About/About.jsx
@@ -4,18 +4,22 @@ const About = () => {
   return (
     <div>
       <h2>About me</h2>
+      <p>about 제작중에 있습니다.</p>
       <p>문화 활동과 휴식을 좋아하는 신입 프론트엔드 개발자 이현민 입니다.</p>
       <p>UX/UI에 관심있는 프론트엔드 주니어 개발자입니다.</p>
       <p>손과 엉덩이로 하는 개발을 추구합니다.</p>
-      <img src='https://ghchart.rshah.org/219138/yhyunmin' alt='이현민 잔디' />
+      <img
+        src='https://ghchart.rshah.org/219138/yhyunmin'
+        alt='이현민 잔디'
+      />{' '}
       <p>
-        포트폴리오 확인은{' '}
+        포트폴리오는{' '}
         <span
           onClick={() => window.open('http://github.com/yhyunmin', '_blank')}
           style={{ fontWeight: 700, cursor: 'pointer' }}>
           깃헙
         </span>{' '}
-        확인 부탁드립니다.about 제작중에 있습니다.
+        확인 부탁드립니다
       </p>
       <p style={{ fontFamily: 'Hans Calli', fontSize: '64px' }}>Hyunmin Yi</p>
     </div>
